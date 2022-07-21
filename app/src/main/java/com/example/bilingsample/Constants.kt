b60e614de238763647cf88d105c9750593c093d7
sana-20 <sana20.dev@gmail.com>
2022-07-21 10:15:11 +0900

    플레이스토어로 이동 처리

--- app/src/main/java/com/example/bilingsample/Constants.kt
+++ app/src/main/java/com/example/bilingsample/Constants.kt
@@ -10,4 +10,7 @@ object Constants {
 
     val LIST_OF_PRODUCTS = listOf(BASIC_SUB, PREMIUM_SUB)
 
+    const val PLAY_STORE_SUBSCRIPTION_DEEPLINK_URL
+            = "https://play.google.com/store/account/subscriptions?product=%s&package=%s"
+
 }
--- app/src/main/java/com/example/bilingsample/ui/SettingsFragment.kt
+++ app/src/main/java/com/example/bilingsample/ui/SettingsFragment.kt
@@ -1,20 +1,18 @@
 package com.example.bilingsample.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
-import com.example.bilingsample.BillingViewModel
-import com.example.bilingsample.databinding.FragmentBasicBinding
+import com.example.bilingsample.Constants
 import com.example.bilingsample.databinding.FragmentSettingsBinding
 
-class SettingsFragment: Fragment() {
+class SettingsFragment : Fragment() {
 
     private lateinit var binding: FragmentSettingsBinding
-
-    private val viewModel: BillingViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,5 +21,19 @@ class SettingsFragment: Fragment() {
     ): View {
         binding = FragmentSettingsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.button.setOnClickListener {
+            val url = String.format(
+                Constants.PLAY_STORE_SUBSCRIPTION_DEEPLINK_URL,
+                Constants.BASIC_SUB,
+                "com.example.bilingsample"
+            )
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
+        }
     }
 }
